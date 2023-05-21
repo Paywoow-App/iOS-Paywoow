79b4a4c7ae06c626803d118af5b1512b7bb2ebcd
Mert Türedü <nomotetes.onetrue@icloud.com>
2023-05-22 01:01:03 +0300

    Uploaded Paywoow Manager

--- PayWoow/PayWoow/Funcs/LocalSecurity.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LocalSecurity.swift
-//  Customer
-//
-//  Created by İsa Yılmaz on 5/12/22.
-//
-
-import Foundation
--- PayWoow/Pods/Target Support Files/CreditCardReader/CreditCardReader-dummy 2.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CreditCardReader : NSObject
-@end
-@implementation PodsDummy_CreditCardReader
-@end
